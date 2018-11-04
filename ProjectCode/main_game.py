9857e472954a9aa8698e79f93f718be05d96b988
ParkJM(NoteBook) <39212289+LEGO0937@users.noreply.github.com>
2018-11-04 16:31:53 +0900

    project 5주차 구현
    아이템을 구현해봤으며 리소스를 추가하였다 추후에 아이템을 먹을시 아이템을 사용할 수 있게 구현할 것이다

--- ProjectCode/main_game.py
+++ ProjectCode/main_game.py
@@ -38,7 +38,7 @@ class Back2:
             self.back2_x = 1350
 
     def draw(self):
-        self.image.draw(self.back2_x,300)
+        self.image.draw(self.back2_x, 300)
 
 class Weapone:
     def __init__(self):
@@ -46,8 +46,32 @@ class Weapone:
         self.yy = 0
         self.state = False
 
-#class Item:
-#    def __init__(selfs):
+class Item:
+
+    image = None
+
+    def __init__(self):
+        if Item.image == None:
+            Item.image = load_image('루기아 깃털.png')
+        self.x = 800
+        self.y = 300
+
+
+    def draw(self):
+            self.image.clip_draw(50, 0, 50, 50, self.x, self.y)
+
+    def update(self):
+        self.x -= random.randint(0, 10)
+        self.y += random.randint(-10, 10)
+        if self.x <= 0:
+            self.x = random.randint(900, 2000)
+            self.y = random.randint(80, 570)
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 25, self.y - 25, self.x + 25, self.y + 25
 
 class Trainer:
 
@@ -64,6 +88,7 @@ class Trainer:
     trainer_image = None
     trainer_attack_image = None
     trainer_weapone_image = None
+    trainer_item_image = None
 
     def __init__(self):
         self.x = 50
@@ -79,10 +104,14 @@ class Trainer:
         self.downon = False
         self.state = False  # enter 'a' key(image change)
         #self.attstate = False   # 트레이너 무기 state
+        self.item = 3
+        self.fire_item = False
 
         self.life_time = 0.0
         self.total_frames = 0.0
         self.dir = 1
+
+        self.summon_assist = False
 
         self.god_time = 0
         self.collide = False
@@ -93,6 +122,7 @@ class Trainer:
             Trainer.trainer_attack_image = load_image('이슬이.png')
         if Trainer.trainer_weapone_image == None:
             Trainer.trainer_weapone_image = load_image('몬스터볼무기.png')
+
 
     def handle_event(self, event):
         if event.type == SDL_KEYDOWN:
@@ -111,6 +141,8 @@ class Trainer:
                 trainer.count += 1
                 if (trainer.count >= 20):
                     trainer.count = 0
+            elif event.key == SDLK_s:
+                trainer.fire_item = True
 
         if event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
@@ -175,6 +207,72 @@ class Assist:
     TIME_PER_ACTION = 0.5
     ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
     FRAMES_PER_ACTION = 4
+
+    def __init__(self):
+        self.x = Trainer.x
+        self.y = Trainer.y
+        self.count = 0  # 트레이너 weapone count
+        self.weap = [Weapone() for i in range(20)]  #펫의 weapone
+        self.frame = 0  # 트레이너 frame
+        self.framea = 0  # 트레이너 weapone frame
+
+        self.life_time = 0.0
+        self.total_frames = 0.0
+        self.dir = 1
+
+        self.god_time = 0
+        self.collide = False
+
+        if Trainer.trainer_image == None:
+            Trainer.trainer_image = load_image('.png')
+        if Trainer.trainer_attack_image == None:
+            Trainer.trainer_attack_image = load_image('이슬이.png')
+        if Trainer.trainer_weapone_image == None:
+            Trainer.trainer_weapone_image = load_image('몬스터볼무기.png')
+
+
+    def update(self, frame_time):
+
+        self.frame += int(self.total_frames) % 20
+        self.framea += int(self.total_frames) % 20
+        self.life_time += frame_time
+        distance = trainer.RUN_SPEED_PPS * frame_time
+        self.total_frames += trainer.FRAMES_PER_ACTION * trainer.ACTION_PER_TIME * frame_time
+
+        if self.righton == True and self.x < 860:
+            self.x += (self.dir * distance)
+        if self.lefton == True and self.x > 40:
+            self.x -= (self.dir * distance)
+        if self.upon == True and self.y < 560:
+            self.y += (self.dir * distance)
+        if self.downon == True and self.y > 30:
+            self.y -= (self.dir * distance)
+        for i in range(20):
+            if self.weap[i].state == True:
+                self.weap[i].xx += (self.dir * distance * 1.3)
+
+    def draw(self):
+        if self.state == True:
+            self.trainer_attack_image.clip_draw(((self.frame % 3) * 70), 0, 70, 70, self.x, self.y)
+        if self.state == False:
+            self.trainer_attack_image.clip_draw(((self.frame % 3) * 70), 0, 70, 70, self.x, self.y)
+        for i in range(20):
+            if self.weap[i].state == True:
+                self.trainer_weapone_image.clip_draw((self.framea % 4) * 30, 0, 30, 30, self.weap[i].xx,
+                                                     self.weap[i].yy)
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 35, self.y - 20, self.x + 25, self.y + 30
+
+    def draw_aa(self, i):
+        draw_rectangle(*self.get_aa(i))
+
+    def get_aa(self, i):
+        return self.weap[i].xx - 10, self.weap[i].yy - 10, self.weap[i].xx - 10, self.weap[i].yy + 12
+
 
 
 
@@ -281,6 +379,7 @@ class Boss:
     FRAMES_PER_ACTION = 7
 
     image = None
+
     def __init__(self):
         if Boss.image == None:
             Boss.image = load_image('fire2.png')
@@ -290,7 +389,7 @@ class Boss:
         self.dir = 0.5
         self.x, self.y = 1200, 300
 
-    def update(self,frame_time):
+    def update(self, frame_time):
         self.frame += int(self.total_frames) % 7
         self.life_time += frame_time
         distance = trainer.RUN_SPEED_PPS * frame_time
@@ -298,20 +397,26 @@ class Boss:
 
         if score_count >= 400:
             self.x -= (self.dir * distance)
+            self.y -= random.randint(-10, 10)
 
         if self.x <= 700:
             self.x = 700
-            self.y = 300
+            #self.y = 300
+        if self.y <= 0:
+            self.y = 0
+        elif self.y >= 600:
+            self.y = 600
 
     def draw(self):
         if score_count >= 400:
+            #self.image.clip_draw((self.frame % 46) * 217, 0, 217, 182, self.x, self.y)
             self.image.clip_draw((self.frame % 3) * 140, 0, 140, 100, self.x, self.y)
 
     def draw_bb(self):
         draw_rectangle(*self.get_bb())
 
     def get_bb(self):
-        return self.x - 100, self.y - 160, self.x + 110, self.y + 210
+        return self.x - 30, self.y - 30, self.x + 30, self.y + 30
 
 class Boss_monster:
 
@@ -383,10 +488,12 @@ def handle_events(frame_time):
             trainer.handle_event(event)
 
 def enter():
-    global back1, back2, trainer, live_font ,enemy1, enemy2, enemies1, time, enemies2, score_count, second_collision, boss, boss1, collision_line, line, boss_monster, boss2, clear_count, warning_font,score_font
+    global back1, back2, trainer, live_font, item, enemy1, items, enemy2, enemies1, time, enemies2, score_count, second_collision, boss, boss1, collision_line, line, boss_monster, boss2, clear_count, warning_font,score_font
     back1 = Back1()
     back2 = Back2()
     trainer = Trainer()
+    item = Item()
+    items = [Item() for i in range(3)]  # 아이템
     enemy1 = Monster1()
     enemies1 = [Monster1() for i in range(25)]  # 몬스터1
     enemy2 = Monster2()
@@ -402,17 +509,17 @@ def enter():
     second_collision = 0        # 두방에 사라진다
     time = get_time()
 
-    #warning_font = load_font('ENCR10B.TTF', 115)  # warning view
     score_font = load_font('Pokemon Hollow.ttf', 25)  # score view
     live_font = load_font('Pokemon Hollow.ttf', 25)
 
 def exit():
-    global back1, back2, enemy1, enemy2, boss
+    global back1, back2, enemy1, enemy2, boss, item
     del(back1)
     del(back2)
     del(enemy1)
     del(enemy2)
     del(boss)
+    del(item)
 
 def collide1(a, b):                             #트레이너와, 적 collision
     left_a, bottom_a, right_a, top_a = a.get_bb()
@@ -445,7 +552,7 @@ def collide2(a, b, i):                          #트레이너와, 적 collision
     return True
 
 def update(frame_time):
-    global score_count, second_collision, die_delay, clear_count
+    global score_count, second_collision, die_delay, clear_count, item_score
     back1.update(frame_time)
     back2.update(frame_time)
     trainer.update(frame_time)
@@ -457,6 +564,8 @@ def update(frame_time):
         boss.update(frame_time)
     for boss_monster in boss2:
         boss_monster.update(frame_time)
+    for item in items:
+        item.update()
 
     for i in range(20):
         for enemy1 in enemies1:
@@ -503,6 +612,31 @@ def update(frame_time):
         if clear_count >= 30:
             game_framework.change_state(clear_state)
 
+    if trainer.fire_item == True:
+        item_score = score_count
+        for i in range(20):
+            for enemy1 in enemies1:
+                    if enemy1.x <= 800 and enemy1.x >= 0:
+                        score_count += 10  # score+
+                        trainer.attstate = False
+                        enemies1.remove(enemy1)
+                        if score_count - item_score > 150:
+                            trainer.fire_item = False
+
+        for i in range(20):
+            for enemy2 in enemies2:
+                if enemy2.x <= 800 and enemy2.x >= 0:
+                    second_collision += 20
+                    trainer.attstate = False
+                    enemy2.state = True
+                    trainer.weap[i].yy = 1000
+                    if score_count - item_score > 150:
+                        trainer.fire_item = False
+                    if second_collision >= 21:
+                        score_count += 20  # score+
+                        second_collision = 0
+                        enemies2.remove(enemy2)
+
 
 def draw(frame_time):
     hide_cursor()
@@ -510,6 +644,7 @@ def draw(frame_time):
     back1.draw()
     back2.draw()
     trainer.draw()
+
 
 
     for enemy1 in enemies1:
@@ -520,20 +655,22 @@ def draw(frame_time):
         boss.draw()
     for boss_monster in boss2:
         boss_monster.draw()
+    for item in items:
+        item.draw()
 
     score_font.draw(700, 570, 'Score: ' + str(score_count), (0, 0, 255))
     live_font.draw(500, 570, 'Live: ' + str(trainer.diecount), (255, 0, 0))
 
     for enemy1 in enemies1:             
         if collide1(trainer, enemy1):
-            #sleep(1)
+            #sleep(0.01)
             trainer.diecount -= 1
             if trainer.diecount <= 0:
                 game_framework.change_state(game_over_state)
 
     for enemy2 in enemies2:           
         if collide1(trainer, enemy2):
-            #sleep(1)
+            #sleep(0.01)
             trainer.diecount -= 1
             if trainer.diecount <= 0:
                 game_framework.change_state(game_over_state)
@@ -541,15 +678,23 @@ def draw(frame_time):
     for boss in boss1:                 
         if collide1(trainer, boss):
             trainer.diecount -= 1
-        if trainer.diecount <= 0:
-            game_framework.change_state(game_over_state)
+            if trainer.diecount <= 0:
+                game_framework.change_state(game_over_state)
 
     for boss_monster in boss2:
         if collide1(trainer, boss_monster):
-            sleep(0.02)
+            #sleep(0.02)
             trainer.diecount -= 1 
             if trainer.diecount <= 0:
                 game_framework.change_state(game_over_state)
 
+    for item in items:
+        if collide1(trainer, item):
+            trainer.item += 1
+            if trainer.item >= 3:
+                trainer.item = 3
+
     update_canvas()
     delay(0.001)
+
+
--- ProjectCode/title_state.py
+++ ProjectCode/title_state.py
@@ -1,5 +1,4 @@
 import game_framework
-import main_game
 import how
 from pico2d import *
 
@@ -8,8 +7,11 @@ name = "TitleState"
 image = None
 
 def enter():
-    global image,bgm
-    image = load_image('메인화면수정.png')
+    global main_image, press_key, frame, real_frame
+    main_image = load_image('메인화면수정.png')
+    press_key = load_image('press_any_button.png')
+    frame = 0
+    real_frame = 0
 
 def exit():
     global image
@@ -29,19 +31,20 @@ def handle_events(frame_time):
                 game_framework.change_state(how)
 
         if event.type == SDL_MOUSEMOTION:
-            x,y = event.x, 600 - event.y
-        #if event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
-        #    if 590< x and x <870 and 480<y and y<540:
-        #        game_framework.change_state(main_game)
-        #    if 590< x and x <870 and 380<y and y<440:
-        # #       game_framework.change_state(how)
+            x, y = event.x, 600 - event.y
+
 def draw(frame_time):
     clear_canvas()
-    image.draw(450,300)
+    main_image.draw(450, 300)
+    #press_key.clip_draw((real_frame % 2) * 500, 150, 500, 150, 450, 100)
+    press_key.draw(450, 100)
     update_canvas()
 
 def update(frame_time):
+    global frame, real_frame
     show_cursor()
+    frame = (frame + 1)
+    real_frame = frame % 100
 
 def pause():
     pass
